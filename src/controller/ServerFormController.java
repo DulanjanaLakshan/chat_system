32d5f8bb5c05d42394c31666d476f59b3e7fefce
DulanjanaLakshan <dulanjana20013@gmail.com>
2022-08-09 12:32:41 +0530

    create chat appliction

--- src/controller/ServerFormController.java
+++ src/controller/ServerFormController.java
@@ -1,4 +1,41 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerFormController {
+    public TextArea textAria;
+    public TextField txtServerMassege;
+    Socket accept=null;
+
+    public void initialize(){
+        new Thread(()->{
+           try {
+               ServerSocket serverSocket=new ServerSocket(5000);
+               System.out.println("Server Started!");
+               accept=serverSocket.accept();
+               System.out.println("Client Connected");
+               InputStreamReader inputStreamReader = new InputStreamReader(accept.getInputStream());
+               BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+               String recode = bufferedReader.readLine();
+               System.out.println(recode);
+           }catch (Exception e){
+
+           }
+        });
+    }
+
+    public void btnSendOnAction(ActionEvent actionEvent) throws IOException {
+        PrintWriter printWriter = new PrintWriter(accept.getOutputStream());
+        printWriter.println(txtServerMassege.getText());
+        printWriter.flush();
+    }
 }
