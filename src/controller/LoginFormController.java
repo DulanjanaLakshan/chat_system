8e0bbfcda4d72dd64cc390209460bcd976e6c205
DulanjanaLakshan <dulanjana20013@gmail.com>
2022-08-10 09:08:59 +0530

    update

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -1,4 +1,51 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginFormController {
+    public TextField txtUsername;
+    public PasswordField txtPassword;
+
+    private final String n_user = "nipuna";
+    private final String n_password = "1234";
+
+    private final String o_user = "oshada";
+    private final String o_password = "admin";
+    Stage primaryStage;
+
+    public void loginOnAction(ActionEvent actionEvent) throws IOException {
+        if (n_user == txtUsername.toString() && n_password == txtPassword.toString()) {
+            Parent load = FXMLLoader.load(getClass().getResource("../views/NipunaForm.fxml"));
+            Scene scene = new Scene(load);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.show();
+        }else if (o_user == txtUsername.toString() && o_password == txtPassword.toString()){
+            Parent load = FXMLLoader.load(getClass().getResource("../views/OshadaForm.fxml"));
+            Scene scene = new Scene(load);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.show();
+        }else {
+            Parent load = FXMLLoader.load(getClass().getResource("../views/NipunaForm.fxml"));
+            Scene scene = new Scene(load);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.show();
+        }
+    }
+
+    public void closeOnAction(ActionEvent actionEvent) {
+    }
 }
--- src/controller/NipunaFormController.java
+++ src/controller/NipunaFormController.java
@@ -1,4 +1,14 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class NipunaFormController {
+    public TextField txtMassage;
+    public TextArea txtAria;
+
+    public void btnMassageOnAction(ActionEvent actionEvent) {
+
+    }
 }
