2ea00ee1386094e4ed6781a0b40845d3a362bf88
DulanjanaLakshan <dulanjana20013@gmail.com>
2022-08-18 20:06:48 +0530

    update

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -2,49 +2,41 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.PasswordField;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class LoginFormController {
-    public TextField txtUsername;
-    public PasswordField txtPassword;
+    public AnchorPane root;
+    public AnchorPane loginForm;
+    public TextField txtUserName;
 
-    private final String n_user = "nipuna";
-    private final String n_password = "1234";
-
-    private final String o_user = "oshada";
-    private final String o_password = "admin";
+    static  String username;
 
     public void loginOnAction(ActionEvent actionEvent) throws IOException {
-        if (n_user == txtUsername.toString() && n_password == txtPassword.toString()) {
-            Parent load = FXMLLoader.load(getClass().getResource("../views/NipunaForm.fxml"));
-            Scene scene = new Scene(load);
-            Stage stage = new Stage();
-            stage.setScene(scene);
-            stage.setResizable(false);
-            stage.show();
-        }else if (o_user == txtUsername.toString() && o_password == txtPassword.toString()){
-            Parent load = FXMLLoader.load(getClass().getResource("../views/OshadaForm.fxml"));
-            Scene scene = new Scene(load);
-            Stage stage = new Stage();
-            stage.setScene(scene);
-            stage.setResizable(false);
-            stage.show();
-        }else {
-            Parent load = FXMLLoader.load(getClass().getResource("../views/NipunaForm.fxml"));
-            Scene scene = new Scene(load);
-            Stage stage = new Stage();
-            stage.setScene(scene);
-            stage.setResizable(false);
-            stage.show();
+        username=txtUserName.getText();
+        if (username.equals("")) {
+            new Alert(Alert.AlertType.WARNING, "Please check your Username").showAndWait();
+            clearloginOnAction();
+
+        } else {
+            Stage stage=(Stage) txtUserName.getScene().getWindow();
+            stage.close();
+            Stage stage1=new Stage();
+            stage1.setScene(new Scene(FXMLLoader.load(getClass().getResource("../views/Client1Form.fxml"))));
+            stage1.setResizable(false);
+            stage1.setTitle(username);
+            stage1.centerOnScreen();
+            stage1.show();
         }
     }
+    public void clearloginOnAction() {
+        txtUserName.clear();
 
-    public void closeOnAction(ActionEvent actionEvent) {
     }
+
 }
